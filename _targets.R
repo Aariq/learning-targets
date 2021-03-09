b2f804859fc64c695bba292cebb264730d287396
Aariq <scottericr@gmail.com>
2021-03-09 15:41:31 -0500

    try .Rmd

--- _targets.R
+++ _targets.R
@@ -1,9 +1,9 @@
 ## Load your packages, e.g. library(targets).
 source("./packages.R")
-source("R/functions.R")
+## Load your R files
+lapply(list.files("./R", full.names = TRUE), source)
 options(tidyverse.quiet = TRUE)
-options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("biglm", "tidyverse"))
+tar_option_set()
 list(
   tar_target(
     raw_data_file,
@@ -20,17 +20,6 @@ list(
       mutate(Ozone = replace_na(Ozone, mean(Ozone, na.rm = TRUE)))
   ),
   tar_target(hist, create_plot(data)),
-  tar_target(fit, biglm(Ozone ~ Wind + Temp, data))
+  tar_target(fit, biglm(Ozone ~ Wind + Temp, data)),
+  tar_render(analysis, "doc/analysis.Rmd")
 )
-
-# ## Load your R files
-# lapply(list.files("./R", full.names = TRUE), source)
-# 
-# ## tar_plan supports drake-style targets and also tar_target()
-# tar_plan(
-# 
-# # target = function_to_make(arg), ## drake style
-# 
-# # tar_target(target2, function_to_make2(arg)) ## targets style
-# 
-# )
